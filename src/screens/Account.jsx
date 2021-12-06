31ccf54b4c3afe71d197a8f673e6045973b48192
javierbeltran425 <00019315@uca.edu.sv>
2021-12-06 14:52:35 -0600

    Using Yup and Formik for input validations

--- src/screens/Account.jsx
+++ src/screens/Account.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import LoginForm from "../components/Auth/LoginForm";
+import UserData from "../components/Auth/UserData";
+
 export default function Account() {
+  const auth = null;
   return (
-    <SafeAreaView>
-      <Text>Account</Text>
-    </SafeAreaView>
+    <View>
+      {auth ? <UserData /> : <LoginForm />}
+    </View>
   );
 }
---
+++ src/utils/userDB.jsx
@@ -0,0 +1,11 @@
+export const user = {
+  username: "javier26",
+  password: "123456",
+};
+
+export const userDetails = {
+    username: "javier26",
+    firstname: "Javier",
+    lastname: "Beltran",
+    email: "uncorreo@gmail.com"
+}
